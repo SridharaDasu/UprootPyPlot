094d7e3f9fee43362987480c61b986929fa7b7bb
Sridhara Dasu <SridharaDasu@gmail.com>
2023-02-25 08:05:32 -0600

    Added example to find dimuon events and print the invariant mass of top two muon pairs

---
+++ print_dimuon_mass.py
@@ -0,0 +1,34 @@
+import sys
+import uproot
+import numpy as np
+import vector
+import matplotlib.pyplot as plt
+
+if len(sys.argv) == 2:
+    input_file = sys.argv[1]
+else:
+    input_file = "c3-zh-aa-bbtautau.root"
+
+input = uproot.open(input_file)
+
+tree = input["Delphes;1"]
+
+# Select events with at least two muons
+for dimuons in tree.arrays(["Muon/Muon.PT", "Muon/Muon.Eta", "Muon/Muon.Phi"]):
+    if len(dimuons["Muon/Muon.PT"]) > 1:
+        # Construct four vectors of the two muons in the event
+        muon0 = vector.obj(
+            pt=dimuons['Muon/Muon.PT'][0],
+            eta=dimuons['Muon/Muon.Eta'][0],
+            phi=dimuons['Muon/Muon.Phi'][0],
+            mass=0.105
+        )
+        muon1 = vector.obj(
+            pt=dimuons['Muon/Muon.PT'][1],
+            eta=dimuons['Muon/Muon.Eta'][1],
+            phi=dimuons['Muon/Muon.Phi'][1],
+            mass=0.105
+        )
+        # Print invariant mass
+        dimuon = muon0 + muon1
+        print(dimuon.mass)
